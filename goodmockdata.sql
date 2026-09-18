d8e76014b0a0395f505c09a87ba7da3173166bd9
Almos Marton Bakonyi <almosbakonyi177@gmail.com>
2026-09-18 14:27:49 +0200

    Corrected th table names in inserts.

--- goodmockdata.sql
+++ goodmockdata.sql
@@ -1,4 +1,4 @@
-INSERT INTO 'AccidentType' Values
+INSERT INTO AccidentType Values
 (1, 'Collision with vehicle', 1),
 (2, 'Collision with pedestrian', 2),
 (3, 'Collision with cyclist', 3),
@@ -6,7 +6,7 @@ INSERT INTO 'AccidentType' Values
 (5, 'Hit by falling object', 5);
 
 
-INSERT INTO 'Bicycle' Values
+INSERT INTO Bicycle Values
 ('B001', 'Giant', 0, 0, 5),
 ('B005', 'Giant', 0, 0, 3),
 ('B002', 'Trek', 1, 0, 2),
@@ -14,7 +14,7 @@ INSERT INTO 'Bicycle' Values
 ('B004', 'Cannondale', 0, 0, 6)
 ;
 
-INSERT INTO 'Cyclist' Values
+INSERT INTO Cyclist Values
 (361738163, 25, 1),
 (361738161, 30, 0),
 (361738169, 22, 1),
@@ -22,7 +22,7 @@ INSERT INTO 'Cyclist' Values
 (361738121, 35, 1)
 ;
 
-INSERT INTO 'DownfallType' Values
+INSERT INTO DownfallType Values
 (1, 'Rain'),
 (2, 'Snow'),
 (3, 'Hail'),
@@ -30,7 +30,7 @@ INSERT INTO 'DownfallType' Values
 (5, 'Fog'),
 (6, 'Drizzle');
 
-INSERT INTO 'ReasonType' Values
+INSERT INTO ReasonType Values
 (1, 'Slippery road'),
 (2, 'Poor visibility'),
 (3, 'Mechanical failure'),
@@ -38,14 +38,14 @@ INSERT INTO 'ReasonType' Values
 (5, 'Animal crossing')
 ;
 
-INSERT INTO 'RoadType' Values
+INSERT INTO RoadType Values
 (1, 'Asphalt', 5),
 (2, 'Concrete', 6),
 (3, 'Gravel', 4),
 (4, 'Dirt', 3),
 (5, 'Cobblestone', 2);
 
-INSERT INTO 'BicycleOwnership' Values
+INSERT INTO BicycleOwnership Values
 (1, 361738163, 'B001'),
 (2, 361738169, 'B002'),
 (3, 361738160, 'B003'),
@@ -53,7 +53,7 @@ INSERT INTO 'BicycleOwnership' Values
 (5, 361738161, 'B005')
 ;
 
-INSERT INTO 'Location' Values
+INSERT INTO Location Values
 (1, 'Amsterdam', 'Damrak', 7, 1),
 (2, 'Rotterdam', 'Coolsingel', 6, 2),
 (3, 'Utrecht', 'Oudegracht', 5, 3),
@@ -61,7 +61,7 @@ INSERT INTO 'Location' Values
 (5, 'Groningen', 'Vismarkt', 3, 5)
 ;
 
-INSERT INTO 'Accident' Values
+INSERT INTO Accident Values
 (1, '2024-01-15 08:30:00', 1, 1, 1, 1, 20, 0),
 (2, '2024-02-20 14:45:00', 2, 2, 2, 2, 15, 1),
 (3, '2024-03-10 18:15:00', 3, 3, 3, 3, 10, 0),
@@ -69,7 +69,7 @@ INSERT INTO 'Accident' Values
 (5, '2024-05-12 16:30:00', 5, 5, 5, 5, 30, 0)
 ;
 
-INSERT INTO 'CyclistAccident' Values
+INSERT INTO CyclistAccident Values
 (1, 361738163, 1, 1, 'B001', 0, 0),
 (2, 361738169, 2, 2, 'B002', 1, 1),
 (3, 361738160, 3, 3, 'B003', 0, 0),
