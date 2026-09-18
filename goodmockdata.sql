5763619e68b041eaeb54439c73604551054117d3
Almos Marton Bakonyi <almosbakonyi177@gmail.com>
2026-09-18 12:42:21 +0200

    Added 3 more table inserts

--- goodmockdata.sql
+++ goodmockdata.sql
@@ -8,10 +8,10 @@ INSERT INTO 'Bicycle' Values
 
 INSERT INTO 'Cyclist' Values
 (361738163, 25, 1),
-(361738164, 30, 0),
-(361738165, 22, 1),
-(361738166, 28, 0),
-(361738167, 35, 1)
+(361738161, 30, 0),
+(361738169, 22, 1),
+(361738160, 28, 0),
+(361738121, 35, 1)
 ;
 
 INSERT INTO 'DownfallType' Values
@@ -28,4 +28,27 @@ INSERT INTO 'ReasonType' Values
 (3, 'Mechanical failure'),
 (4, 'Human error'),
 (5, 'Animal crossing')
+;
+
+INSERT INTO 'RoadType' Values
+(1, 'Asphalt', 5),
+(2, 'Concrete', 6),
+(3, 'Gravel', 4),
+(4, 'Dirt', 3),
+(5, 'Cobblestone', 2);
+
+INSERT INTO 'BicycleOwnership' Values
+(1, 361738163, 'B001'),
+(2, 361738169, 'B002'),
+(3, 361738160, 'B003'),
+(4, 361738121, 'B004'),
+(5, 361738161, 'B005')
+;
+
+INSERT INTO 'Location' Values
+(1, 'Amsterdam', 'Damrak', 7, 1),
+(2, 'Rotterdam', 'Coolsingel', 6, 2),
+(3, 'Utrecht', 'Oudegracht', 5, 3),
+(4, 'Eindhoven', 'Stratumseind', 4, 4),
+(5, 'Groningen', 'Vismarkt', 3, 5)
 ;
